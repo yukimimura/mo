82235accb2d08d7d4a6584f70e6e54ea475c11aa
yuki.mimura <perusona5excuse@yahoo.co.jp>
2020-04-19 17:03:07 +0000

    image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   
+  mount_uploader :image, ImageUploader
+  
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 1000 }
   validates :review, presence: true
